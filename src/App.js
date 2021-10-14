ec5475b94c4cf337d59d6aaf14b6c3220e40eb7c
Franklin <franklin.rosembluth@ucb.edu.bo>
2021-10-14 02:13:56 -0400

    Debería devolver la dirección del auto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-
-function carPosition(position) {
-  var x = position.split(",")[0];
-  var y = position.split(",")[1];
-  if (position == "") {
-    x = "0";
-    y = "0";
+function sanatizePosition(position) {
+  let positionSplited = position.split(",");
+  var x = positionSplited[0];
+  var y = positionSplited[1];
+  var direction = position.substr(3, 4);
+  if (position === "") {
+    x = "0"; y = "0"; direction = "";
   }
-  return x + "," + y;
+  return [x, y, direction];
 }
 
-module.exports = {carPosition}
+function carPosition(position) {
+  var listOfPosition = sanatizePosition(position);
+  return parseInt(listOfPosition[0]), parseInt(listOfPosition[1]);
+}
+function carDirection(position) {
+  var listOfPosition = sanatizePosition(position);
+  return listOfPosition[2];
+}
+
+module.exports = {carPosition, sanatizePosition, carDirection}
